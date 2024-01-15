1e5037db4c2ae73a15426b0b25e28ade9a262f0a
kennan12 <kenanusheta24@gmail.com>
2024-01-15 11:48:12 -0800

    web_dynamics

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$('input[type=checkbox]').click(function () {
+		const myListName = [];
+		const myId = [];
+		$('input[type=checkbox]:checked').each(function ()
+			myListName.push($(this).attr('data-name'));
+			myId.push($(this).attr('data-id'));
+	});
+		if (yListName.length === 0) {
+			$('.amenities h4').html('&nbsp;');
+		} else {
+			$('.amenities h4').text(myListName.join(', '));
+		}
+		console.log(myId);
+});
